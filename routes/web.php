32dd874931c4c578e6d0a11f1fc9b69e18b97f52
cschibelsky-star <cschibelsky@gmail.com>
2026-08-22 13:00:05 -0300

    feat(cultura): protect dashboard and add profile and opportunity routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\CulturaDashboardController;
+use App\Http\Controllers\CulturalOpportunityController;
+use App\Http\Controllers\CulturalProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,4 +17,9 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/cultura', CulturaDashboardController::class)->name('cultura.dashboard');
+Route::middleware('auth')->prefix('cultura')->name('cultura.')->group(function () {
+    Route::get('/', CulturaDashboardController::class)->name('dashboard');
+    Route::get('/perfil', [CulturalProfileController::class, 'edit'])->name('profile.edit');
+    Route::put('/perfil', [CulturalProfileController::class, 'update'])->name('profile.update');
+    Route::get('/oportunidades/{opportunity}', [CulturalOpportunityController::class, 'show'])->name('opportunities.show');
+});
