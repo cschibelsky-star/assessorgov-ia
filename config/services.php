42431f9d1757abf3bb3ca08ed2ad5529be299019
cschibelsky-star <cschibelsky@gmail.com>
2026-09-01 16:14:54 -0300

    feat(billing): configure Asaas API client

--- config/services.php
+++ config/services.php
@@ -2,6 +2,9 @@
 
 return [
     'asaas' => [
+        'api_url' => env('ASAAS_API_URL', 'https://api-sandbox.asaas.com/v3'),
+        'api_key' => env('ASAAS_API_KEY'),
         'webhook_token' => env('ASAAS_WEBHOOK_TOKEN'),
+        'timeout' => (int) env('ASAAS_TIMEOUT', 15),
     ],
 ];
