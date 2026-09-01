4d8f4a51c550ea9f15d06dba49c380d1d8380f29
cschibelsky-star <cschibelsky@gmail.com>
2026-09-01 07:14:29 -0300

    config: add PNCP consultation settings

--- config/services.php
+++ config/services.php
@@ -4,4 +4,12 @@ return [
     'asaas' => [
         'webhook_token' => env('ASAAS_WEBHOOK_TOKEN'),
     ],
+
+    'pncp' => [
+        'base_url' => env('PNCP_BASE_URL', 'https://pncp.gov.br/api/consulta'),
+        'timeout' => (int) env('PNCP_TIMEOUT', 20),
+        'connect_timeout' => (int) env('PNCP_CONNECT_TIMEOUT', 5),
+        'retry_attempts' => (int) env('PNCP_RETRY_ATTEMPTS', 3),
+        'retry_sleep_ms' => (int) env('PNCP_RETRY_SLEEP_MS', 750),
+    ],
 ];
