09d0966e78f4ccbb6328a7e67ec3f63a880c1d45
cschibelsky-star <cschibelsky@gmail.com>
2026-09-10 15:53:08 -0300

    feat(transferegov): include fund-to-fund public module

--- config/transferegov.php
+++ config/transferegov.php
@@ -3,8 +3,10 @@
 return [
     'base_url' => env('TRANSFEREGOV_BASE_URL', 'https://api-publica.transferegov.gestao.gov.br'),
     'timeout' => (int) env('TRANSFEREGOV_TIMEOUT', 20),
+    'page_size' => (int) env('TRANSFEREGOV_PAGE_SIZE', 200),
     'modules' => [
         'parcerias' => '/parcerias',
         'especiais' => '/especiais',
+        'fundoafundo' => '/fundoafundo',
     ],
 ];
