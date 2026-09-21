f1b3298a074e061c620d369d39bfc6e182481646
cschibelsky-star <cschibelsky@gmail.com>
2026-09-21 09:38:18 -0300

    feat: add compliance resolution routes

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\CulturalProfileController;
 use App\Http\Controllers\GovIntelligenceController;
 use App\Http\Controllers\GovIntelligenceActionController;
 use App\Http\Controllers\GovComplianceController;
+use App\Http\Controllers\GovComplianceActionController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home')->name('home');
@@ -38,6 +39,8 @@ Route::middleware('auth')->prefix('app')->name('gov.')->group(function () {
     Route::get('/inteligencia', GovIntelligenceController::class)->name('intelligence');
     Route::post('/inteligencia/{item}/aplicar', [GovIntelligenceActionController::class, 'store'])->name('intelligence.apply');
     Route::get('/compliance', GovComplianceController::class)->name('compliance');
+    Route::put('/compliance/{item}', [GovComplianceActionController::class, 'update'])->name('compliance.update');
+    Route::get('/compliance/{customerOpportunity}/{item}/evidencia', [GovComplianceActionController::class, 'evidence'])->name('compliance.evidence');
 });
 
 Route::middleware('auth')->prefix('cultura/app')->name('cultura.')->group(function () {
